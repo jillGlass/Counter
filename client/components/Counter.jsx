8f680274fe305bfcd2120d6ed33b5770f668913f
JillGlass <glass.nz@gmail.com>
2020-04-05 22:09:35 +1200

    setState count increment

--- client/components/Counter.jsx
+++ client/components/Counter.jsx
@@ -7,29 +7,33 @@ class Counter extends React.Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
-  handleIncrement = () => {
-      console.log('yup!')
-  }
-
   render() {
     return (
       <React.Fragment>
-        {this.state.tags.length === 0 && <p>Please create a new tag</p>}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <Button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </Button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  handleIncrement = () => {
+    this.setState({count: this.state.count +1})
+  };
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
