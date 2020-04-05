a82abc6918967eeef39f0bcd17820647a3fb3f4c
JillGlass <glass.nz@gmail.com>
2020-04-05 21:06:46 +1200

    dynamically render button classes

--- client/components/Counter.jsx
+++ client/components/Counter.jsx
@@ -3,26 +3,32 @@ import { Button } from "react-bootstrap";
 
 class Counter extends React.Component {
   state = {
-    count: 0,
-    
+    count: 0
   };
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
-  }
 
   
 
   render() {
+   
+
     return (
       <React.Fragment>
-         
-        <span className='badge badge-primary m-2'>{this.formatCount()}</span>
-        <Button className='btn btn-secondary btn-sm'>Increment</Button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <Button className="btn btn-secondary btn-sm">Increment</Button>
       </React.Fragment>
     );
   }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
+      }
 }
 
 export default Counter;
