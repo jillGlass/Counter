f631f05ce16cfa1e6eb50788312916b0cc52fdd2
JillGlass <glass.nz@gmail.com>
2020-04-05 22:01:55 +1200

    add handle increment

--- client/components/Counter.jsx
+++ client/components/Counter.jsx
@@ -4,35 +4,33 @@ import { Button } from "react-bootstrap";
 class Counter extends React.Component {
   state = {
     count: 0,
-    tags:['tag1', 'tag2', 'tag3']
+    tags: ["tag1", "tag2", "tag3"]
   };
 
-  
-
-  render() {
-   
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
     return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <Button className="btn btn-secondary btn-sm">Increment</Button>
-        <ul>
-            {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
-      </React.Fragment>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
     );
   }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
+  handleIncrement = () => {
+      console.log('yup!')
+  }
 
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
-      }
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.tags.length === 0 && <p>Please create a new tag</p>}
+        {this.renderTags()}
+      </React.Fragment>
+    );
+  }
 }
 
 export default Counter;
